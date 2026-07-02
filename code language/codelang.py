26b55881e60b44cac44ea34698793a0f7ef052ac
Aayusshman Baral <b.aayushman@op.iitg.ac.in>
2026-07-02 23:04:15 +0530

    codelang.py
    this is the codebase for the code language where you add a certain character sequence when our user input starts with a character and when it starts with a consonant it adds another sequence of characters in this

---
+++ code language/codelang.py
@@ -0,0 +1,14 @@
+def codelanguade():
+    print('We have to add yay at the end it string starts with a vowel and ay if it srats with consonant')
+    st=str(input('Enter the string of your choice:'))
+    vowel='yay'
+    const='ay'
+    if st[0]=='a' or st[0]=='e' or st[0]=='i' or st[0]=='o' or st[0]=='u':
+        print('yes')
+        c=st+vowel
+        final.append(c)
+    x=st+const
+    final.append(x)
+    print(final)
+codelanguade()
+            
